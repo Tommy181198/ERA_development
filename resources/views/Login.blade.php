dd63f68fa1a9667f9ba4cff94d1b4f2a8dc124f6
Geraldus Tommy <gtommyk.1811@gmail.com>
2020-06-22 16:41:04 +0700

    init login

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/cerulean/bootstrap.min.css">
     <title>Login</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="jumbotron">
+            <h1>test</h1>
+        </div>
+    </div>
 </body>
 </html>
