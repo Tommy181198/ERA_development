e3ad3677772474a6b80e4dd2c1ae4583a1b2e70a
Geraldus Tommy <gtommyk.1811@gmail.com>
2020-06-23 14:48:28 +0700

    Responsive page (Belum selesai)

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/cerulean/bootstrap.min.css"> --}}
     <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width",initial-scale=1.0>
     <title>Login</title>
 </head>
 <body>
