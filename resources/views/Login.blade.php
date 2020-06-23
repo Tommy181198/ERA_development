324b220696797534bcbc70c7f44bcdf5a137ba24
Geraldus Tommy <gtommyk.1811@gmail.com>
2020-06-23 12:03:07 +0700

    Login Page UI Design

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -4,13 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/cerulean/bootstrap.min.css">
+    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/cerulean/bootstrap.min.css"> --}}
+    <link rel="stylesheet" href="css/style.css">
     <title>Login</title>
 </head>
 <body>
     <div class="container">
-        <div class="jumbotron">
-            <h1>test</h1>
+        <div class="form-container">
+            <div class="form-body">
+                <div class="header">
+                    <h2>Login</h2>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <input type = "text" placeholder = "Username">
+                    </div>
+                    <div class="input-group">
+                        <input type = "password" placeholder = "Password">
+                    </div>
+                    <div class="input-group flex">
+                        <div class="forgot"><a href="#">Forgot Password?</a></div>
+                    </div>
+                    <div class="input-group">
+                        <button>LOGIN</button>
+                    </div>
+                </div>
+            </div>
+            <div class="form-image">
+                <div class="text">
+                    <h2>Welcome <br> To </h2>
+                <h3>Emergency Roadside Assistance Attendance & Unit Monitoring App</h3>
+                </div>
+            </div>
         </div>
     </div>
 </body>
